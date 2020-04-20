8a41590977af3e4d23627b4c6286690fbe2422ff
Elmar Athmer <elmar.athmer@rewe-digital.com>
2020-04-20 23:56:47 +0200

    add migrations to DatabaseSettings

--- core/src/db/settings.rs
+++ core/src/db/settings.rs
@@ -1,28 +1,39 @@
-use std::time::Duration;
-use diesel::PgConnection;
-use diesel::r2d2::ConnectionManager;
 use crate::db::Pool;
-use r2d2::Error;
+use diesel::migration::RunMigrationsError;
+use diesel::r2d2::ConnectionManager;
+use diesel::{Connection, ConnectionResult, PgConnection};
+use std::time::Duration;
+use thiserror::Error;
 
-pub struct DatabaseSettings {
-    pub database_url: String,
+#[derive(Error, Debug)]
+pub enum RappDbError {
+    #[error(transparent)]
+    RunMigrationsError(#[from] RunMigrationsError),
+    #[error(transparent)]
+    R2d2Error(#[from] r2d2::Error),
 
-    /// see https://docs.rs/r2d2/0.8.7/r2d2/struct.Builder.html#method.min_idle
-    pub min_idle: Option<u32>,
-
-    /// see https://docs.rs/r2d2/0.8.7/r2d2/struct.Builder.html#method.idle_timeout
-    pub idle_timeout: Option<Duration>,
-
-    /// see https://docs.rs/r2d2/0.8.7/r2d2/struct.Builder.html#method.max_size
-    pub max_size: u32,
+    #[error("No migrations defined")]
+    NoMigrationsError,
 }
 
+type MigrationRunner = fn(&PgConnection) -> Result<(), RunMigrationsError>;
 
+pub struct DatabaseSettings {
+    database_url: String,
+    migration_runner: Option<MigrationRunner>,
+    /// see https://docs.rs/r2d2/0.8.7/r2d2/struct.Builder.html#method.min_idle
+    min_idle: Option<u32>,
+    /// see https://docs.rs/r2d2/0.8.7/r2d2/struct.Builder.html#method.idle_timeout
+    idle_timeout: Option<Duration>,
+    /// see https://docs.rs/r2d2/0.8.7/r2d2/struct.Builder.html#method.max_size
+    max_size: u32,
+}
 
 impl DatabaseSettings {
     pub fn new(database_url: String) -> Self {
         DatabaseSettings {
             database_url,
+            migration_runner: None,
             // settings are tuned to use as few connections as possible,
             // since available connections are rather limited:
             // https://cloud.google.com/sql/docs/quotas
@@ -32,7 +43,13 @@ impl DatabaseSettings {
         }
     }
 
-    pub fn establish_pooled_connection(&self) -> Result<Pool, Error> {
+    pub fn new_with_migrations(database_url: String, func: MigrationRunner) -> Self {
+        let mut s = Self::new(database_url);
+        s.migration_runner = Some(func);
+        s
+    }
+
+    fn get_pool(&self) -> Result<Pool, r2d2::Error> {
         let manager = ConnectionManager::<PgConnection>::new(&*self.database_url);
 
         Pool::builder()
@@ -41,6 +58,34 @@ impl DatabaseSettings {
             .idle_timeout(self.idle_timeout)
             .test_on_check_out(true)
             .build(manager)
+    }
+
+    /// get a database-connection pool and also run migrations, so the database/pool/connection
+    /// is ready to use
+    pub fn establish_pooled_connection(&self) -> Result<Pool, RappDbError> {
+        let pool = self.get_pool()?;
+
+        if let Some(r) = self.migration_runner {
+            let connection = pool.get()?;
+            r(&connection)?;
+        }
+
+        Ok(pool)
+    }
+
+    /// establish a single connection. Useful for tests etc.
+    pub fn establish_connection(&self) -> ConnectionResult<PgConnection> {
+        PgConnection::establish(&self.database_url)
+    }
+
+    pub fn run_migrations(&self, connection: &PgConnection) -> Result<(), RappDbError> {
+        let migrations = self
+            .migration_runner
+            .ok_or(RappDbError::NoMigrationsError)?;
+
+        migrations(connection)?;
+
+        Ok(())
     }
 }
 
